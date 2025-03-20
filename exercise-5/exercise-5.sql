a961bb2b24f6cf3b9e45e4e6b5abd5e4f871f1fb
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-20 14:26:57 +0700

    take exercise 5

---
+++ exercise-5/exercise-5.sql
@@ -0,0 +1,3 @@
+-- Liệt kê danh sách các khách hàng trong bảng Customers nhưng không có đơn hàng nào trong bảng Orders.
+
+-- Hiển thị tên khách hàng (CompanyName) và tổng doanh thu họ đã chi tiêu, sắp xếp giảm dần và chỉ lấy khách hàng chi tiêu nhiều nhất.
