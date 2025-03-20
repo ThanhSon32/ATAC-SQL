e3b7dcb655015bcfd4610e8b2dcff50da7be6f87
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-20 14:39:33 +0700

    add SQL

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,3 +1,10 @@
 -- Liệt kê danh sách các sản phẩm (Products) cùng với tên danh mục (Categories) và tên nhà cung cấp (Suppliers).
+SELECT a.ProductName, b.CategoryName, c.CompanyName
+FROM Products a
+INNER JOIN Categories b on a.CategoryID = b.CategoryID
+INNER JOIN Suppliers c on a.SupplierID = c.SupplierID
 
 -- Tìm tất cả các khách hàng (Customers) có tên công ty chứa chữ "Market"
+SELECT *
+FROM Customers
+WHERE CompanyName LIKE '%Market%';
