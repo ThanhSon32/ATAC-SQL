f660c8c71b25003f1e49f28e4821220e9bcac652
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-20 14:22:53 +0700

    take exercise 3

---
+++ exercise-3/exercise-3.sql
@@ -0,0 +1,3 @@
+-- Liệt kê danh sách các sản phẩm (Products) cùng với tên danh mục (Categories) và tên nhà cung cấp (Suppliers).
+
+-- Tìm tất cả các khách hàng (Customers) có tên công ty chứa chữ "Market"
