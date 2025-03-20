4a745b4a3521f03cbce0f081ce61a862dc48b7db
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-20 15:18:52 +0700

    add SQL query

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,3 +1,15 @@
 -- Liệt kê danh sách các khách hàng trong bảng Customers nhưng không có đơn hàng nào trong bảng Orders.
+SELECT a.CustomerID
+FROM Customers a
+INNER JOIN Orders b ON a.CustomerID = b.CustomerID
+GROUP BY a.CustomerID
+HAVING COUNT(b.OrderID) = 0;
 
 -- Hiển thị tên khách hàng (CompanyName) và tổng doanh thu họ đã chi tiêu, sắp xếp giảm dần và chỉ lấy khách hàng chi tiêu nhiều nhất.
+SELECT a.CompanyName, Sum(c.UnitPrice)
+FROM Customers a
+INNER JOIN Orders b ON a.CustomerID = b.CustomerID
+INNER JOIN 'Order Details' c ON b.OrderID = c.OrderID
+GROUP BY a.CustomerID
+ORDER BY Sum(c.UnitPrice) DESC
+LIMIT 1
