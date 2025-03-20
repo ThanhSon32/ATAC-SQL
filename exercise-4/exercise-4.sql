d79361701d73c139953d687f729c7ae1631a06b7
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-20 14:25:11 +0700

    take exercise 4

---
+++ exercise-4/exercise-4.sql
@@ -0,0 +1,4 @@
+-- Cập nhật (UPDATE) giá sản phẩm tăng 10% cho tất cả các sản phẩm thuộc danh mục có tên là "Beverages".
+
+-- Thêm (INSERT) một sản phẩm mới có tên "Organic Coffee" và bảng Products, thuộc danh mục "Beverages", 
+--có giá 25.00, số lượng tồn kho là 100, và được cung cấp bởi một nhà cung cấp (Suppliers) bất kỳ
