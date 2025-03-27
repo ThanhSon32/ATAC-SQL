fadf5e9c22cfe83e9a1c10872d1b0d69fe1ff214
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-27 09:12:18 +0700

    Correct the wrong query

--- exercise-5/exercise-5.sql
+++ exercise-5/exercise-5.sql
@@ -1,15 +1,14 @@
 -- Liệt kê danh sách các khách hàng trong bảng Customers nhưng không có đơn hàng nào trong bảng Orders.
-SELECT a.CustomerID
-FROM Customers a
-INNER JOIN Orders b ON a.CustomerID = b.CustomerID
-GROUP BY a.CustomerID
-HAVING COUNT(b.OrderID) = 0;
+SELECT c.CustomerID
+FROM Customers c
+LEFT JOIN Orders o ON c.CustomerID = o.CustomerID
+WHERE o.OrderID IS NULL;
 
 -- Hiển thị tên khách hàng (CompanyName) và tổng doanh thu họ đã chi tiêu, sắp xếp giảm dần và chỉ lấy khách hàng chi tiêu nhiều nhất.
 SELECT a.CompanyName, Sum(c.UnitPrice*c.Quantity)
 FROM Customers a
 INNER JOIN Orders b ON a.CustomerID = b.CustomerID
-INNER JOIN 'Order Details' c ON b.OrderID = c.OrderID
+INNER JOIN "Order Details" c ON b.OrderID = c.OrderID
 GROUP BY a.CompanyName
 ORDER BY Sum(c.UnitPrice*c.Quantity) DESC
 LIMIT 1
