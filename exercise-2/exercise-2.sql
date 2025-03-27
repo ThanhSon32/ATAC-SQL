1c1912451309a8eb7b39f979dc5a265415139fc8
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-27 08:55:24 +0700

    Optimize performance

--- exercise-2/exercise-2.sql
+++ exercise-2/exercise-2.sql
@@ -1,11 +1,11 @@
 -- Tính tổng doanh thu (UnitPrice * Quantity) của từng khách hàng dựa trên bảng Orders và Order Details.
 SELECT a.CustomerID, SUM(b.UnitPrice * b.Quantity) AS TotalRevenue
-FROM Orders a, "Order Details" b
-WHERE a.OrderID = b.OrderID
-GROUP BY a.CustomerID
+FROM Orders a
+JOIN "Order Details" b ON a.OrderID = b.OrderID
+GROUP BY a.CustomerID;
 
 -- Hiển thị danh sách 5 sản phẩm (Products) có giá cao nhất (UnitPrice).
-SELECT * 
+SELECT ProductName, UnitPrice
 FROM Products 
 ORDER BY UnitPrice DESC 
 LIMIT 5;
