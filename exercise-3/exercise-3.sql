728254573e0cfaf98fdbe05e2f0ee028eabb1766
ThanhSon32 <thanhson.uit@outlook.com>
2025-03-27 09:01:47 +0700

    Optimize reader

--- exercise-3/exercise-3.sql
+++ exercise-3/exercise-3.sql
@@ -1,8 +1,8 @@
 -- Liệt kê danh sách các sản phẩm (Products) cùng với tên danh mục (Categories) và tên nhà cung cấp (Suppliers).
-SELECT a.ProductName, b.CategoryName, c.CompanyName
-FROM Products a
-INNER JOIN Categories b on a.CategoryID = b.CategoryID
-INNER JOIN Suppliers c on a.SupplierID = c.SupplierID
+SELECT p.ProductName, c.CategoryName, s.CompanyName
+FROM Products p
+INNER JOIN Categories c ON p.CategoryID = c.CategoryID
+INNER JOIN Suppliers s ON p.SupplierID = s.SupplierID;
 
 -- Tìm tất cả các khách hàng (Customers) có tên công ty chứa chữ "Market"
 SELECT *
